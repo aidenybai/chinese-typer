3e37d5f04239fdc4995cbd727371b557a9690562
Aiden Bai <mail.contraband@gmail.com>
2019-06-15 16:41:58 -0700

    Update cookie handling and styles

--- public/js/client.js
+++ public/js/client.js
@@ -245,8 +245,40 @@ var wordList = [
   "your","yourself","youth","zero","zebra","zipper","zoo","zulu"
 ];
 
-$('#textarea').val(document.cookie);
+function setCookie(name,value,days) {
+    var expires = "";
+    if (days) {
+        var date = new Date();
+        date.setTime(date.getTime() + (days*24*60*60*1000));
+        expires = "; expires=" + date.toUTCString();
+    }
+    document.cookie = name + "=" + (value || "")  + expires + "; path=/";
+}
+function getCookie(name) {
+    var nameEQ = "txt-" + name + "=";
+    var ca = document.cookie.split(';');
+    for(var i=0;i < ca.length;i++) {
+        var c = ca[i];
+        while (c.charAt(0)==' ') c = c.substring(1,c.length);
+        if (c.indexOf(nameEQ) == 0) return c.substring(nameEQ.length,c.length);
+    }
+    return null;
+}
+function eraseCookie(name) {   
+    document.cookie = name+'=; Max-Age=-99999999;';  
+}
 
-setInterval(function() {
-  document.cookie = $('#textarea').val();
-}, 60000);
+function saved() {
+  var ca = document.cookie.split(';');
+  console.log(ca)
+  ca.forEach((saved) => {
+    if (saved.startsWith(' txt-')) {
+      $('#saved').val($('#saved').val() + '\n' + saved.split('=')[0].replace(' txt-', ''));
+    }
+  })
+}
+
+var input = prompt('Load Previous Document?');
+$('#textarea').val(getCookie(input));
+
+saved();
